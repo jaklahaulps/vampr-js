ed0865ad909e6632076ee917b9b116009cc4737d
jaklahaulps <liu.perry@gmail.com>
2019-01-02 14:38:18 -0500

    update

--- vampire.js
+++ vampire.js
@@ -10,21 +10,38 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let aCounter = 0; 
+    function findParent (vampireName){
+      if (vampireName.creator !== null) {
+        let currentVamp = vampireName.creator;
+        aCounter++;
+        return findParent(currentVamp);
+      } else {
+        return aCounter
+      }
+    }
+    findParent(this)
+    return aCounter
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal){
+      return true;
+    } else {
+      return false;
+    }
 
   }
 
